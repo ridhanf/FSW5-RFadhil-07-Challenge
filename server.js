7ef30bbe558ecd70bf6e57fe0d5e8c20cc909df4
Ridhan Fadhilah <ridhanfadhilah@gmail.com>
2021-02-17 19:02:27 +0700

    server

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const app = require('./app');
-const http = require('http');
-
 const PORT = process.env.PORT || 3000;
-const server = http.createServer(app);
 
 // Running Server
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.info(`Server listening on port ${PORT} (http://localhost:${PORT})`)
 });
