06e7f75917323ea4de7e66e6f9ced013d74164f9
Ridhan Fadhilah <ridhanfadhilah@gmail.com>
2021-03-01 18:55:21 +0700

    encrypt password

--- db/seeders/20210208190516-User.js
+++ db/seeders/20210208190516-User.js
@@ -1,4 +1,5 @@
 'use strict';
+const bcrypt = require('bcrypt');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
@@ -7,7 +8,7 @@ module.exports = {
         id: '06896bd4-8cbc-48c6-8c46-9364a6d939c4',
         username: 'rfadhil',
         email: 'ridhanfadhilah@gmail.com',
-        password: '12345678',
+        password: bcrypt.hashSync('12345678', 10),
         createdAt: new Date(),
         updatedAt: new Date()
       },
@@ -15,7 +16,7 @@ module.exports = {
         id: '6dcb3b28-d798-4cec-80b5-516bfe18c25b',
         username: 'vkurniawan',
         email: 'vkurniawan@binar.co.id',
-        password: '12345678',
+        password: bcrypt.hashSync('12345678', 10),
         createdAt: new Date(),
         updatedAt: new Date()
       },
@@ -23,7 +24,7 @@ module.exports = {
         id: '7bbe267d-e1aa-41d5-9a48-50e74fc04400',
         username: 'sabrina',
         email: 'sabrina@binar.co.id',
-        password: '12345678',
+        password: bcrypt.hashSync('12345678', 10),
         createdAt: new Date(),
         updatedAt: new Date()
       }
