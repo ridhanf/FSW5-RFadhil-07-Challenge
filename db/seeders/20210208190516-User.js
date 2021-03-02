6107652c351bd34838334c948ef3e2230a427868
Ridhan Fadhilah <ridhanfadhilah@gmail.com>
2021-03-02 23:23:27 +0700

    add Admin

--- db/seeders/20210208190516-User.js
+++ db/seeders/20210208190516-User.js
@@ -5,6 +5,14 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('user_game', [
       {
+        id: 'd649bb89-0e21-4fdc-ad4e-e19f78c59f07',
+        username: 'admin',
+        email: 'admin@rfadhil.com',
+        password: bcrypt.hashSync('admin', 10),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
         id: 'e7516524-7b5c-11eb-9439-0242ac130002',
         username: 'binar',
         email: 'binar@binar.co.id',
